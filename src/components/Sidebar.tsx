e2d12c68389788703c04efeac4170946b0cc301c
Claudionor <claudionorojr@hotmail.com>
2023-03-29 12:42:00 -0300

    Initialized repository

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,31 @@
+import { PencilLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
+
+import avatarImg from '../assets/me.jpeg'
+
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1518839283416-0cc546d12a97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <Avatar src={avatarImg} />
+
+        <strong>Claudionor Ojr</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
